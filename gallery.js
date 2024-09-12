e3ee977a3bcd41c51a1fdb3f7939ef3f992ea59f
Pawel4050 <paweubielinski@gmail.com>
2024-09-12 10:54:28 +0200

    up

--- gallery.js
+++ gallery.js
@@ -89,7 +89,7 @@ function onKeyDown(event) {
   }
 }
 
-let instance = null; // Zmienna globalna
+let instance = null;
 
 gallery.addEventListener("click", (event) => {
   event.preventDefault();
@@ -97,151 +97,19 @@ gallery.addEventListener("click", (event) => {
   if (link) {
     const largeImageLink = link.href;
     console.log(largeImageLink);
-
-    // Przypisanie instancji do zmiennej globalnej 'instance'
     instance = basicLightbox.create(
       `
         <img width="1400" height="900" src="${largeImageLink}">
       `,
       {
         onShow: () => {
-          // Dodanie nasłuchiwania zdarzenia po otwarciu okna modalnego
           window.addEventListener("keydown", onKeyDown);
         },
         onClose: () => {
-          // Usunięcie nasłuchiwania zdarzenia po zamknięciu okna modalnego
           window.removeEventListener("keydown", onKeyDown);
         },
       }
     );
-
     instance.show();
   }
 });
-
-// const gallery = document.querySelector(".gallery");
-
-// const images = [
-//   {
-//     preview:
-//       "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",
-//     original:
-//       "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",
-//     description: "Hokkaido Flower",
-//   },
-//   {
-//     preview:
-//       "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
-//     original:
-//       "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
-//     description: "Container Haulage Freight",
-//   },
-//   {
-//     preview:
-//       "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
-//     original:
-//       "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
-//     description: "Aerial Beach View",
-//   },
-//   {
-//     preview:
-//       "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
-//     original:
-//       "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
-//     description: "Flower Blooms",
-//   },
-//   {
-//     preview:
-//       "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
-//     original:
-//       "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
-//     description: "Alpine Mountains",
-//   },
-//   {
-//     preview:
-//       "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
-//     original:
-//       "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
-//     description: "Mountain Lake Sailing",
-//   },
-//   {
-//     preview:
-//       "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
-//     original:
-//       "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
-//     description: "Alpine Spring Meadows",
-//   },
-//   {
-//     preview:
-//       "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
-//     original:
-//       "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
-//     description: "Nature Landscape",
-//   },
-//   {
-//     preview:
-//       "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
-//     original:
-//       "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
-//     description: "Lighthouse Coast Sea",
-//   },
-// ];
-
-// const galleryItems = images.map((image) => {
-//   return `
-//     <li class="gallery-item">
-//       <a class="gallery-link" href="${image.original}">
-//         <img
-//           class="gallery-image"
-//           src="${image.preview}"
-//           data-source="${image.original}"
-//           alt="${image.description}"
-//         />
-//       </a>
-//     </li>
-//   `;
-// });
-
-// gallery.innerHTML = galleryItems.join("");
-
-// function onKeyDown(event) {
-//   if (event.key === "Escape" && instance) {
-//     instance.close();
-//   }
-// }
-
-// let instance = null;
-
-// gallery.addEventListener("click", (event) => {
-//   event.preventDefault();
-//   const link = event.target.closest("a");
-//   if (link) {
-//     const largeImageLink = link.href;
-//     console.log(largeImageLink);
-
-//     const instance = basicLightbox.create(
-//       `
-//         <img width="1400" height="900" src="${largeImageLink}">
-//       `,
-//       {
-//         onShow: () => {
-//           // Dodanie nasłuchiwania zdarzenia po otwarciu okna modalnego
-//           window.addEventListener("keydown", onKeyDown);
-//         },
-//         onClose: () => {
-//           // Usunięcie nasłuchiwania zdarzenia po zamknięciu okna modalnego
-//           window.removeEventListener("keydown", onKeyDown);
-//         },
-//       }
-//     );
-
-//     instance.show();
-//     // basicLightbox
-//     //   .create(
-//     //     `
-//     //         <img width="1400" height="900" src="${largeImageLink}">
-//     //     `
-//     //   )
-//     //   .show();
-//   }
-// });
